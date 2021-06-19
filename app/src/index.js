cc0c7b84ba49a09551c62ad022fddfe8a872f85e
Petrit Krasniqi <44484338+Petrit123@users.noreply.github.com>
2021-06-19 10:05:11 +0100

    Update index.js

--- app/src/index.js
+++ app/src/index.js
@@ -8,11 +8,7 @@ import { default as contract } from 'truffle-contract'
 /*
  * When you compile and deploy your Voting contract,
  * truffle stores the abi and deployed address in a json
- * file in the build directory. We will use this information
- * to setup a Voting abstraction. We will use this abstraction
- * later to create an instance of the Voting contract.
- * Compare this against the index.js from our previous tutorial to see the difference
- * https://gist.github.com/maheshmurthy/f6e96d6b3fff4cd4fa7f892de8a1a1b4#file-index-js
+ * file in the build directory. 
  */
 
 import voting_artifacts from '../../build/contracts/Voting.json'

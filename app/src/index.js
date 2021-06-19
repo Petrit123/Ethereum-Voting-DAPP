b217c37abad277cfb3544d3bd821d84e13a4199a
Petrit Krasniqi <44484338+Petrit123@users.noreply.github.com>
2021-06-19 10:06:37 +0100

    Update index.js

--- app/src/index.js
+++ app/src/index.js
@@ -1,4 +1,3 @@
-// Import the page's CSS. Webpack will know what to do with it.
 import "../styles/app.css";
 
 // Import libraries we need.
@@ -20,11 +19,7 @@ window.voteForCandidate = function(candidate) {
   try {
     $("#msg").html("Vote has been submitted. The vote count will increment as soon as the vote is recorded on the blockchain. Please wait.")
     $("#candidate").val("");
-
-    /* Voting.deployed() returns an instance of the contract. Every call
-     * in Truffle returns a promise which is why we have used then()
-     * everywhere we have a transaction call
-     */
+    
     Voting.deployed().then(function(contractInstance) {
       contractInstance.voteForCandidate(candidateName, {gas: 140000, from: account}).then(function() {
         let div_id = candidates[candidateName];
@@ -42,11 +37,11 @@ window.voteForCandidate = function(candidate) {
 $( document ).ready(function() {
   if (typeof web3 !== 'undefined') {
     console.warn("Using web3 detected from external source like Metamask")
-    // Use Mist/MetaMask's provider
+
     window.web3 = new Web3(web3.currentProvider);
   } else {
     console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+
     window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
   }
 
